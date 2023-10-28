f3469079b34c4111989a74989127dabcb86663c3
Griselda Benedicta <134153904+RelaxCat@users.noreply.github.com>
2023-10-28 14:24:03 +0700

    page

--- assets/index-f9800dda.js
+++ assets/index-f9800dda.js
@@ -17370,16 +17370,16 @@ const kw = (e) => {
             element: w.jsx(vc, {}),
           }),
           w.jsx(fr, { path: "./:category", element: w.jsx(vc, {}) }),
-          w.jsx(fr, { path: "./", element: w.jsx(Sw, {}) }),
+          w.jsx(fr, { path: "./CineHarbor/", element: w.jsx(Sw, {}) }),
         ],
       })
     );
   };
 const Dp = "./assets/logo-39f50a69.png",
   yc = [
-    { display: "Home", path: "./" },
-    { display: "Movies", path: "./movie" },
-    { display: "Series", path: "./tv" },
+    { display: "Home", path: "./CineHarbor/" },
+    { display: "Movies", path: "./movie/" },
+    { display: "Series", path: "./tv/" },
   ],
   Mw = () => {
     const { pathname: e } = qn(),
@@ -17410,7 +17410,7 @@ const Dp = "./assets/logo-39f50a69.png",
               className: "logo",
               children: [
                 w.jsx("img", { src: Dp, alt: "" }),
-                w.jsx(ie, { to: "./", children: "CineHarbor" }),
+                w.jsx(ie, { to: "./CineHarbor/", children: "CineHarbor" }),
               ],
             }),
             w.jsx("ul", {
@@ -17458,9 +17458,9 @@ const Rw = () =>
                       className: "footer-content-menu-title",
                       children: "Menus",
                     }),
-                    w.jsx(ie, { to: "/", children: "Home" }),
-                    w.jsx(ie, { to: "/movie", children: "Movies" }),
-                    w.jsx(ie, { to: "/tv", children: "Series" }),
+                    w.jsx(ie, { to: "./CineHarbor/", children: "Home" }),
+                    w.jsx(ie, { to: "./movie/", children: "Movies" }),
+                    w.jsx(ie, { to: "./tv/", children: "Series" }),
                   ],
                 }),
                 w.jsxs("div", {
@@ -17470,9 +17470,9 @@ const Rw = () =>
                       className: "footer-content-menu-title",
                       children: "Genres",
                     }),
-                    w.jsx(ie, { to: "/", children: "Horror" }),
-                    w.jsx(ie, { to: "/", children: "Animation" }),
-                    w.jsx(ie, { to: "/", children: "Fantasy" }),
+                    w.jsx(ie, { to: "./", children: "Horror" }),
+                    w.jsx(ie, { to: "./", children: "Animation" }),
+                    w.jsx(ie, { to: "./", children: "Fantasy" }),
                   ],
                 }),
                 w.jsxs("div", {
@@ -17482,9 +17482,9 @@ const Rw = () =>
                       className: "footer-content-menu-title-genres",
                       children: "Genres",
                     }),
-                    w.jsx(ie, { to: "/", children: "Action" }),
-                    w.jsx(ie, { to: "/", children: "Sci-fi" }),
-                    w.jsx(ie, { to: "/", children: "Romance" }),
+                    w.jsx(ie, { to: "./", children: "Action" }),
+                    w.jsx(ie, { to: "./", children: "Sci-fi" }),
+                    w.jsx(ie, { to: "./", children: "Romance" }),
                   ],
                 }),
               ],
